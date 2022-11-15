6b5f0cbbc2dd3199a444a49af365ec74b211cf7b
Ramiro Nzau <ramirofrancisco962014@gmail.com>
2022-11-15 16:14:29 +0100

    initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,17 @@
+import { Flex } from '@chakra-ui/react';
+import Head from 'next/head';
+
+export default function Home(){
+    
+  return (
+      <>
+        <Head>
+           <title>NFT Access | Sign In</title>
+        </Head>
+        <Flex align='center' justify='center'>
+          <h1>Hello chakra</h1>
+        </Flex>
+      </>
+  )  
+}
+
