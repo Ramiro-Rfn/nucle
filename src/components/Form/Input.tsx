2edcdf49cf2d8dcc53cacaea76204ee42572a697
Ramiro Nzau <ramirofrancisco962014@gmail.com>
2022-11-17 07:43:23 +0100

    creating sign in page

---
+++ src/components/Form/Input.tsx
@@ -0,0 +1,31 @@
+import { FormControl, FormLabel, Input as ChakraInput, InputProps as ChakraInputProps } from '@chakra-ui/react';
+
+interface InputProps extends ChakraInputProps {
+    name: string,
+    label?: string
+}
+export function Input({ name, label }: InputProps) {
+    return(
+        <FormControl>
+            {!!label && <FormLabel htmlFor={name} color='white'>{label}</FormLabel>}
+
+            <ChakraInput 
+                name={name} 
+                type='password'
+                id={name}
+                _placeholder={{
+                color: 'gray.200'
+                }}
+                color='gray.50'
+                placeholder='Digite sua senha'
+                focusBorderColor='green.500'
+                bgColor='gray.900'
+                variant='filled'
+                _hover={{
+                bgColor: 'gray.900'
+                }}
+                size='md'
+            />
+        </FormControl>
+    )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
-import { Flex } from '@chakra-ui/react';
+import { Button, Flex, Stack, Text } from '@chakra-ui/react';
 import Head from 'next/head';
+import { Input } from '../components/Form/Input';
 
 export default function Home(){
     
@@ -8,8 +9,30 @@ export default function Home(){
         <Head>
            <title>Nucle | Home</title>
         </Head>
-        <Flex h='100vh' w='100vw' bg='gray.900'>
-          
+        <Flex 
+          h='100vh' 
+          w='100vw'
+          align='center'
+          justify='center'
+          flexDir='column'
+        >
+          <Text color='green.500' fontSize='3rem' fontWeight='bold' mb='10'>.Nucle</Text>
+
+          <Flex 
+            as='form' 
+            w='100%' 
+            maxW={360} 
+            bg='gray.800'
+            p='8'
+            borderRadius={8}
+            flexDir='column'
+          > 
+            <Stack spacing='4'>
+              <Input name='email' type='email' label='Email'/>
+              <Input name='password' type='password' label='Senha'/>
+            </Stack>
+            <Button type='submit' colorScheme='green' mt='6'>Entrar</Button>
+          </Flex>
         </Flex>
       </>
   )  
