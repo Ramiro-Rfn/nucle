ee29113a9d4dae2a7400f549faf9bc3f78532fe4
Ramiro Nzau <ramirofrancisco962014@gmail.com>
2022-12-03 08:38:33 +0100

    make sidebar responsive

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,19 @@
-import { Flex, useBreakpointValue } from "@chakra-ui/react";
+import { Flex, Icon, IconButton, useBreakpointValue } from "@chakra-ui/react";
+import { RiMenuLine } from "react-icons/ri";
+import { useSidebarDrawer } from "../../context/SidebarDrawerContext";
 import { Logo } from "./Logo";
 import { NotificationsNav } from "./NotificationsNav";
 import { Profile } from "./Profile";
 import { SearchBox } from "./SearchBox";
 
 export function Header() {
+    const {onOpen} = useSidebarDrawer();
 
     const isWideVersion = useBreakpointValue({
         base: false,
         lg: true
-    })
+    });
+
     return(
         <Flex
             as='header'
@@ -22,6 +26,18 @@ export function Header() {
             align='center'
             justify='space-between'
         >
+            {!isWideVersion && (
+                <IconButton
+                    aria-label="Open Navigation"
+                    icon={<Icon as={RiMenuLine}/>}
+                    fontSize='24'
+                    variant='unstyled'
+                    onClick={onOpen}
+                    mr='2'
+                >
+
+                </IconButton>
+            )}
            <Logo/>
 
 
