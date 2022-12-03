b3c4dfc25fcb1c7866786cb5e88e584669d94042
Ramiro Nzau <ramirofrancisco962014@gmail.com>
2022-12-03 10:31:15 +0100

    making create user page be responsive

--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Divider, Flex, Heading, HStack, SimpleGrid, Stack } from "@chakra-ui/react";
+import { Box, Button, Divider, Flex, Heading, SimpleGrid, Stack } from "@chakra-ui/react";
 import { Input } from "../../components/Form/Input";
 import { Header } from "../../components/Header";
 import { Sidebar } from "../../components/Sidebar";
@@ -11,28 +11,28 @@ export default function CreateUser() {
             <Flex w='100%' my='6' maxW={1480} mx='auto' px='6'>
                 <Sidebar/>
 
-                <Box flex='1' borderRadius={8} bg='gray.800' p='8'>
+                <Box flex='1' borderRadius={8} bg='gray.800' p={['6','8']}>
                     <Heading size='lg' fontWeight='normal'>Usuários</Heading>
                     
                     <Divider my='4' borderColor='gray.700'/>
 
-                    <Stack spacing='8'>
-                        <SimpleGrid minChildWidth='240px' spacing='8' w='100%'>
+                    <Stack spacing={['6','8']}>
+                        <SimpleGrid minChildWidth='240px' spacing={['6','8']} w='100%'>
                             <Input  name="name" label="Nome Completo" />
                             <Input  name="email" type='email' label="Email" />
                         </SimpleGrid>
 
-                        <SimpleGrid minChildWidth='240px' spacing='8' w='100%'>
+                        <SimpleGrid minChildWidth='240px' spacing={['6','8']} w='100%'>
                             <Input  name="password" type='password' label="Senha" />
                             <Input  name="password_confimation" type='password' label="Confirmação de senha" />
                         </SimpleGrid>
                     </Stack>
 
                     <Flex mt='8' justify='flex-end'>
-                        <HStack spacing='4'>
+                        <Stack spacing='4' direction='row'>
                             <Button colorScheme='whiteAlpha'>Cancelar</Button>
                             <Button colorScheme='green'>Salvar</Button>
-                        </HStack>
+                        </Stack>
                     </Flex>
 
                 </Box>
